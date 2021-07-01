ce7c070102b7aef7b8906039ab2dfa463d7eee03
safronman <safronmanbox@gmail.com>
2021-07-01 10:18:02 +0300

    add select with css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
     return (
         <div>
             <h1>GIT</h1>
+            <section>
+                <h2>Select</h2>
+                <select name="" id="" className={'select'}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+            </section>
         </div>
 
     );
