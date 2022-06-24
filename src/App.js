d25f02c9ae4cd4a178c88b432df396fcd519d7b8
safronman <safronmanbox@gmail.com>
2022-06-24 20:25:32 +0300

    sidorov-1

--- src/App.js
+++ src/App.js
@@ -4,45 +4,7 @@ import './App.css';
 function App() {
     return (
         <div>
-            <h1>GIT23</h1>
-            <section>
-                <h2>Select</h2>
-                <select name="" id="" className={'select'}>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </select>
-            </section>
-            <section>
-                <h2>Input</h2>
-                <input type="text" className={'input'}/>
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <p>5</p>
-            </section>
-            <section>
-                <h2>CSS1</h2>
-                <span>1</span>
-                <span>2</span>
-                <span>3</span>
-            </section>
-            <h1>dev2 - 1 commit</h1>
-            <h1>dev2 - 2 commit</h1>
-            <h1>dev2 - 3 commit</h1>
-            <h1>dev2 - 4 commit</h1>
-            <h1>dev2 - 5 commit</h1>
-            <h1>dev2 - 6 commit</h1>
-            <h1>dev2 - 7 commit</h1>
-            <h1>dev2 - pr commit - 1</h1>
-            <div>
-                1.Исправление багов, ошибок, конфликтов с другими приложениями,
-                2. Добавление новых функций, возможностей,
-                3. Рефакторинг, стилевые правки
-            </div>
-            <p>1</p>
-            <p>2</p>
+            <p>sidorov page</p>
         </div>
 
     );
