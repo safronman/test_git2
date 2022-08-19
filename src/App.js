401005cedd6a706d8c82f7abf1704a2363138b78
safronman <safronmanbox@gmail.com>
2022-08-19 20:28:51 +0300

    user1-commit-1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
         <div>
             <p>1</p>
             <p>2</p>
+            <div>
+                <h1>Login</h1>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
+            </div>
         </div>
     );
 }
