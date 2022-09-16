51a7af3a4100d5ea912ac9dd0d208d70e21ce639
safronman <safronmanbox@gmail.com>
2022-09-16 20:26:32 +0300

    user1 commit-1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
         <div>
             <p>1</p>
             <p>2</p>
+            <div>
+                login
+                <input type="text" placeholder={'email'}/>
+            </div>
         </div>
     );
 }
