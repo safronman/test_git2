f88632618137f9eb9897e08b0f06c2e5454197dc
safronman <safronmanbox@gmail.com>
2022-09-16 20:23:02 +0300

    start

--- src/App.js
+++ src/App.js
@@ -6,26 +6,6 @@ function App() {
         <div>
             <p>1</p>
             <p>2</p>
-            <div>
-                <h1>Login</h1>
-                <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </ul>
-            </div>
-            <hr/>
-            <div>
-                <h1>Profile</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, non?</p>
-ghfdgfgfdg
-            <p>df
-            fsd
-            sdf</p>
-            </div>
-
-            1111
-            222
         </div>
     );
 }
