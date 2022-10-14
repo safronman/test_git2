a4d48b54010138053eb73e15735b1ecb9a85d738
safronman <safronmanbox@gmail.com>
2022-10-14 20:20:03 +0300

    user-2 commit-1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
     return (
         <div>
-           <h1>Hello</h1>
+            <h1>Profile</h1>
+            <img src="" alt=""/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, nulla.</p>
         </div>
     );
 }
