e93b4f49134c72313cb4077bb79cc1bba5c9f275
safronman <safronmanbox@gmail.com>
2022-12-09 20:12:21 +0300

    user1 commit1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <h1>login</h1>
+      <form action="">
+        <input type="email" />
+      </form>
+    </div>
+  )
 }
 
 export default App
