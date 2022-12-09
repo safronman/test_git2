5ea32861260786554a227e5d431b9046973e0ad8
safronman <safronmanbox@gmail.com>
2022-12-09 20:05:17 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return (
-    <div>
-      <h1>profile</h1>
-      <img src="" alt="" />
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, placeat?</p>
-      <input type="text" />
-      <button>111111</button>
-        4444444444
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
