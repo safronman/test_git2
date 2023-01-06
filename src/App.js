eea8e9df5f0b476bcdd0724ca75ffca76e71cd14
safronman <safronmanbox@gmail.com>
2023-01-06 20:08:27 +0300

    start commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,7 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return (
-    <div>
-      <h1>APP-Andrey</h1>
-      <div>
-        <h1>login</h1>
-        <form action="">
-          <input type="email" />
-        </form>
-        <button>1</button>
-      </div>
-      <div>
-        <h1>profile-Andrey3</h1>
-        <div>
-          <img src="#" alt="" />
-        </div>
-         <button>Gello!</button>
-      </div>
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
