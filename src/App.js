572276a81a60526d44be726fb06b6636b8b7a61a
safronman <safronmanbox@gmail.com>
2023-02-03 20:06:18 +0300

    user1 - 1 commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <h1>login</h1>
+      <input type="text" placeholder={'enter login'} />
+      <button>login</button>
+    </div>
+  )
 }
 
 export default App
