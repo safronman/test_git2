e7c4559b6b198b25fafca5cdf26d7328d1b9c870
Pikadorius <karpukea@yandex.by>
2023-02-03 20:36:24 +0300

    commit Egor

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <button>login</button>
       </div>
       <div>
-        <h1>PROFILE</h1>
+        <h1>PROFILE 2</h1>
         <img src="" alt="" />
       </div>
       <div>registrate</div>
