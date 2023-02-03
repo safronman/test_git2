f851ab5787995a11e40632f9956ce8599362d37a
safronman <safronmanbox@gmail.com>
2023-02-03 20:07:45 +0300

    user2 - 1 commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <h1>PROFILE</h1>
+      <img src="" alt="" />
+    </div>
+  )
 }
 
 export default App
