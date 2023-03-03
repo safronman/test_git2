ad20ea3d6294ee00078a6445ba8313eb52cee224
safronman <safronmanbox@gmail.com>
2023-03-03 20:29:13 +0300

    logout1 - 1 commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react'
+
 const a = 1
 
 function App() {
-  return (
-    <div>
-	    <h1>login</h1>
-	    <input type="text" placeholder={'login'}/>
-	    <h2>profile</h2>
-	    <img src="fdsfds/sdfdsf" alt="avatar"/>
-    </div>
-  )
+
+	const logoutHandler = () => {
+		alert('logout')
+	}
+
+	return (
+		<div>
+			<h1>login</h1>
+			<input type="text" placeholder={'login'}/>
+			<h2>profile</h2>
+			<img src="fdsfds/sdfdsf" alt="avatar"/>
+			<button onClick={logoutHandler}>logout</button>
+		</div>
+	)
 }
 
 export default App
