61005fdca3c4a011d0ec06ae1f5811cc24dd3650
safronman <safronmanbox@gmail.com>
2023-03-03 20:10:20 +0300

    init

--- src/App.js
+++ src/App.js
@@ -4,17 +4,6 @@ const a = 1
 function App() {
   return (
     <div>
-      <div>
-        <h1>master999</h1>
-        <input type="text" placeholder={'enter login'} />
-        <button>login</button>
-      </div>
-      <div>
-        <h1>PROFILE 334342</h1>
-        <img src="" alt="" />
-      </div>
-      <div>registrate</div>
-      asdfsa
     </div>
   )
 }
